c88ab7aaae3c413629e10201b38fa852600c3426
Chris <cliff5345179@gmail.com>
2017-11-19 00:01:18 +0800

    11

--- src/seleniumtestcase1.java
+++ src/seleniumtestcase1.java
@@ -20,12 +20,24 @@ public class seleniumtestcase1 {
         //setup web driver
         System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
         FirefoxDriver driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         String baseUrl = "https://play.kkbox.com/";
         driver.get(baseUrl);
+
+        // setup writer for report
+        reportwriter write = new reportwriter();
+        SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+        Date current = new Date();
+        String result = String.valueOf(current);
+        write.writerTxt(result);
+
+        /**
+         * start of test case 1
+         */
+        //login
         driver.findElement(By.xpath(".//*[@id='uid']")).sendKeys("cliff5345179@gmail.com");
         driver.findElement(By.xpath(".//*[@id='pwd']")).sendKeys("Saki0608");
         driver.findElement(By.xpath(".//*[@id='login-btn']")).click();
+
         //close ads if exist
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         int ads = driver.findElements(By.className("close")).size();
@@ -33,17 +45,127 @@ public class seleniumtestcase1 {
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             driver.findElement(By.className("close")).click();
         }
+
         //find the four bookmarks and make sure they exist
         int sidebarNav = driver.findElements(By.className("sidebar-nav")).size();
+        if (sidebarNav != 0) {
+            int cpl = driver.findElements(By.cssSelector("[ng-click*=cpl]")).size();
+            int explore = driver.findElements(By.cssSelector("[ng-click*=explore]")).size();
+            int radio = driver.findElements(By.cssSelector("[ng-click*=radio]")).size();
+            int listen_with = driver.findElements(By.cssSelector("[ng-click*=listen-with]")).size();
+
+            /**
+             * end of test case 1
+             */
+            if (cpl != 0 && explore != 0 && radio != 0 && listen_with != 0) {
+                result = "test case 1 pass";
+                write.writerTxt(result);
+            } else {
+                result = "test case 1 failed";
+                write.writerTxt(result);
+            }
         }
+
+        /**
+         * start of test case 2
+         */
+
+        // input target
         driver.findElement(By.className("search_hint")).sendKeys("清平調");
+
+        //search
         driver.findElement(By.id("search_btn_cnt")).click();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        //target String
+        String target = "王菲&鄧麗君 (Faye Wong & Teresa Teng) - 清平調 搶先聽";
+
+        //get actual target
+        String check =driver.findElement(By.xpath("//*[@id='container']/div[2]/div/div[1]/div/div/div/div/div[2]/ol/li[2]/div/div[2]/h3/a")).getText();
+        result = check;
+        write.writerTxt(result);
+
+        /**
+         * end of test case 2
+         */
+        if (target.equals(check)) {
+            result = "test case 2 pass";
+            write.writerTxt(result);
+        }
+        else{
+            result = "test case 2 failed";
+            write.writerTxt(result);
+        }
+
+        /**
+         *         start of test case 3
+         */
         driver.findElement(By.cssSelector("[ng-click*=radio]")).click();
+
+        //wait for loading
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        //initiate plying check
+        isRadioPlaying Playing = new isRadioPlaying();
+        Boolean checkPlaying = Playing.isRadioPlaying(driver);
+
+        // initiate mouse hover action
+        Actions action = new Actions(driver);
+        //find radio image in advance
+        WebElement element = driver.findElement(By.xpath("//*[@id='promote-stations']/div/ul/li[1]/div/div[1]"));
+
+        //Play radio with check playing
+        while (!checkPlaying) {
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+            // hover on image so button will show
+            action.moveToElement(element).build().perform();
+
+            //press radio play button
+            driver.findElement(By.xpath("//*[@id='promote-stations']/div/ul/li[1]/div/div[1]/div/a")).click();
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+            // check if playing is true
+            checkPlaying = Playing.isRadioPlaying(driver);
+        }
+
+        //record orginal song
+        String beforeDislike = driver.findElement(By.xpath("//*[@id='container']/div[2]/div/div[1]/div/div/div/div[1]/div/div/div/h3/a")).getText();
+        result = "before dislike:"+beforeDislike;
+        write.writerTxt(result);
+
+        //sleep a while
+        Thread.sleep(3000);
+
+        //press dislike
+        driver.findElement(By.xpath("//*[@title = '不喜歡']")).click();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        //wait until play
+        checkPlaying = Playing.isRadioPlaying(driver);
+        while (!checkPlaying){
+            checkPlaying = Playing.isRadioPlaying(driver);
         }
+
+        //record song afterwards
+        String afterDislike = driver.findElement(By.xpath("//*[@id='container']/div[2]/div/div[1]/div/div/div/div[1]/div/div/div/h3/a")).getText();
+        result = "after dislike:"+afterDislike;
+        write.writerTxt(result);
+
+        /**
+         * end of test case 3
+         */
+        if (beforeDislike.equals(afterDislike)){
+            result = "test case 3 failed";
+            write.writerTxt(result);
         }
+        else{
+            result = "test case 3 pass";
+            write.writerTxt(result);
+        }
+
+        //all done!
+        driver.quit();
     }
 
 }
